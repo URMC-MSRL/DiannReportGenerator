ba7a94fcc5a9972eb15ab3e1f1d7f13a15c4251b
Kyle Swovick <99501054+kswovick@users.noreply.github.com>
2025-02-10 16:23:05 -0500

    Update diann_report.R
    Removed MaxLFQ calculation and updated code.

--- R/diann_report.R
+++ R/diann_report.R
@@ -1,42 +1,13 @@
 #' URMC MSRL DIA-NN Report Generator
 #'
-#' Using the 'report.tsv' generating from a DIA-NN search, creates a new file that includes MaxLFQ and number of peptides in wide format
-#' Requires the DiaNN R package to be installed from github (https://github.com/vdemichev/diann-rpackage) to run properly.
-#' To install follow these steps:
-#'     install.packages("devtools")
-#'     library(devtools)
-#'     isntall_github("https://github.com/vdemichev/diann-rpackage")
+#' Using the 'report.tsv' generated from a DIA-NN search, creates a new file that includes MaxLFQ and number of peptides in wide format
 #'
-#' @import diann
 #' @import dplyr
-#' @import purrr
 #' @import readr
 #' @import stringr
 #' @import tibble
-#' @import tidyr
 #'
 #' @name diann_reporter
-
-#' @param df a data frame from an upstream step
-
-peptide_count <- function(df) {
-   as.data.frame(
-      table(
-         unique(df[,c(
-            'Protein.Group',
-            'Modified.Sequence')])$'Protein.Group'
-      )
-   )
-}
-
-#' @param df a data frame from an upstream step
-
-eliminate_duplicate <- function(df) {
-   dplyr::distinct(df,
-            Protein.Group,
-            .keep_all = T
-   )
-}
 
 #' Reads the report.tsv as input and performs several functions including:
 #'     Filters according to Library Q-Value Library Protein Group Q-Value (both 0.1), PEP (0.5), and removes keratin
@@ -57,75 +28,50 @@ eliminate_duplicate <- function(df) {
 diann_reporter <- function(report_in, report_out
                            ) {
 
-      filtered_diann_report <- diann::diann_load('report.tsv') %>%
-      dplyr::filter(Lib.Q.Value <= 0.01 &
+   filtered_diann_report <- readr::read_tsv('report.tsv') %>%
+    dplyr::filter(Lib.Q.Value <= 0.01 &
                     Lib.PG.Q.Value <= 0.01 &
-                    PEP <= 0.5 &
+                    PEP <= 0.2 &
                     !stringr::str_detect(First.Protein.Description, 
-                                         'Keratin'))
-   
-   maxlfq_matrix <- diann::diann_maxlfq(filtered_diann_report,
-                                        group.header = 'Protein.Group',
-                                        id.header = 'Precursor.Id',
-                                        quantity.header = 'Precursor.Normalised')
-   
-   maxlfq_df <- maxlfq_matrix %>%
-      as.data.frame() %>%
-      tibble::rownames_to_column(var = 'Protein.Group') %>%
-      tibble::as_tibble() %>%
-      tidyr::pivot_longer(cols = 2:last_col(),
-                          names_to = 'File.Name',
-                          values_to = 'MaxLFQ') %>%
-      dplyr::inner_join(x = filtered_diann_report %>%
-                           dplyr::select(-c('PG.Quantity':'Genes.MaxLFQ.Unique', 
-                                            'Stripped.Sequence':'Predicted.iRT', 
-                                            'Ms1.Profile.Corr':'Predicted.iIM')),
-                        y = .,
-                        by = c('File.Name', 
-                               'Protein.Group'),
-                        na_matches = 'na') %>%
-      dplyr::select(-c('File.Name', 
-                       'Protein.Ids',
-                       'Protein.Names',
-                       'Lib.Q.Value',
-                       'Lib.PG.Q.Value')) %>%
-      dplyr::relocate(First.Protein.Description, .after = Genes) %>%
-      tidyr::nest(data = c(Protein.Group:MaxLFQ))
-   
-   df_joined <- dplyr::left_join(x = maxlfq_df %>%
-                                    tidyr::unnest(data) %>%
-                                    dplyr::select(-c('Modified.Sequence')) %>%
-                                    dplyr::rename('Sample.Name' = Run),
-                                 y = maxlfq_df %>%
-                                    dplyr::mutate(n.peptides = purrr::map(data, 
-                                                                          peptide_count)) %>%
-                                    dplyr::select(c('Run', 
-                                                    'n.peptides')) %>%
-                                    tidyr::unnest(n.peptides)  %>%
-                                    purrr::set_names('Sample.Name',
-                                                     'Protein.Group',
-                                                     'Number.Peptides'),
-                                 by = c("Sample.Name",
-                                        "Protein.Group")) %>%
-      dplyr::group_by(Sample.Name) %>%
-      tidyr::nest() %>%
-      dplyr::mutate(end.data = purrr::map(data, 
-                                          eliminate_duplicate)) %>%
-      dplyr::select(-c(data)) %>%
-      tidyr::unnest(end.data)
-   
-   fin_df <- dplyr::inner_join(x = df_joined %>%
-                                  dplyr::select(-c(6)) %>%
-                                  tidyr::pivot_wider(names_from = Sample.Name,
-                                                     values_from = MaxLFQ),
-                               y = df_joined %>%
-                                  dplyr::select(c(1:2, 6)) %>%
-                                  tidyr::pivot_wider(names_from = Sample.Name,
-                                                     values_from = Number.Peptides,
-                                                     names_prefix = 'Number.Peptides'),
-                               by = 'Protein.Group') %>%
-      dplyr::rename('Protein.Accession' = Protein.Group,
-                    'Protein.Name' = First.Protein.Description)
-
-   write_tsv(fin_df, report_out)
+                                         'Keratin')) %>% 
+    dplyr::mutate(Run = sub("^[^_]*_([^_]*).*", 
+                            "\\1",
+                            Run)) 
+  
+  n_peptides <- filtered_diann_report %>% 
+    dplyr::distinct(Run,
+                    Protein.Group,
+                    Genes,
+                    First.Protein.Description,
+                    Precursor.Id,
+                    Modified.Sequence) %>% 
+    dplyr::select(-Precursor.Id) %>% 
+    dplyr::group_by(Run,
+                    Protein.Group,
+                    Genes,
+                    First.Protein.Description) %>% 
+    dplyr::summarise(value = n()) %>% 
+    dplyr::mutate(variable = 'NumberPeptides')
+  
+ maxlfq <- filtered_diann_report %>% 
+   dplyr::distinct(Run,
+                   Protein.Group,
+                   Genes, 
+                   First.Protein.Description,
+                   PG.MaxLFQ) %>% 
+   dplyr::rename('value' = PG.MaxLFQ) %>% 
+   dplyr::mutate(variable = 'MaxLFQ')
+  
+ data_wide <- dplyr::bind_rows(x = n_peptides,
+                               y = maxlfq) %>% 
+   dplyr::pivot_wider(names_from = variable,
+                      values_from = value) %>% 
+   dplyr::pivot_wider(names_from = Run,
+                      values_from = c(NumberPeptides,
+                                      MaxLFQ)) %>% 
+   dplyr::rename('UniProt Accession' = Protein.Group,
+                 'Gene Name' = Genes,
+                 'Protein Name' = First.Protein.Description)
+  
+  readr::write_tsv(data_wide, report_out)
 }
